d379df2ac5a3695e807ab3c5bd90f2f18aa0c0b4
ZedZed34 <haun070@aucklanduni.ac.nz>
2026-01-14 23:09:28 +0700

    Docker is running

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"log"
-	// "os"  <-- DELETE THIS LINE
+
 	"github.com/spf13/viper"
 )
 
@@ -20,11 +20,21 @@ func LoadConfig() (Config, error) {
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 
+	// 1. Set Defaults
 	viper.SetDefault("DB_HOST", "db")
 	viper.SetDefault("DB_PORT", "5432")
 	viper.SetDefault("HOSPITAL_A_API_URL", "https://hospital-a.api.co.th")
 	viper.SetDefault("JWT_SECRET", "change_this_secret_in_prod")
 
+	// 2. EXPLICITLY BIND ENV VARS (Critical Fix)
+	// Viper often ignores env vars if it doesn't know the key exists yet.
+	_ = viper.BindEnv("DB_USER")
+	_ = viper.BindEnv("DB_PASSWORD")
+	_ = viper.BindEnv("DB_NAME")
+	_ = viper.BindEnv("DB_HOST")
+	_ = viper.BindEnv("DB_PORT")
+
+	// If running in Docker, these env vars will overwrite defaults
 	if err := viper.ReadInConfig(); err != nil {
 		log.Println("Config file not found, using environment variables")
 	}
